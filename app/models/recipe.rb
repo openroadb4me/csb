01a2801f95cd83e196a56a12c40108c70e775438
Nathan Anderson <nathan.anderson87@gmail.com>
2018-03-22 14:22:56 +0400

    Final

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,4 +1,5 @@
 class Recipe < ApplicationRecord
+  belongs_to :user
   has_many :ingredients, dependent: :destroy
   has_many :directions, dependent: :destroy
   accepts_nested_attributes_for :ingredients,
